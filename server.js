7b1d53639fb3bfeab344712098316ab04fdca8ea
luke2134 <127568286+luke2134@users.noreply.github.com>
2023-12-07 16:05:20 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Simulated database
+const users = [
+    { username: 'user', password: 'password' }
+];
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check against the simulated database (replace this with your database logic)
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+// Serve Landing Page.html for the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Landing Page.html'));
+});
+
+// Handle other routes by serving the corresponding HTML files
+app.get('/:page', (req, res) => {
+    const page = req.params.page;
+    res.sendFile(path.join(__dirname, 'public', `${page}.html`));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+});
